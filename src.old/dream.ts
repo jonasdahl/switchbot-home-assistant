64ca4fe0ad29d50497f9688f5a129e137cdb5a5d
Jonas Dahl <jonas.dahl@yanzinetworks.com>
2022-06-23 00:10:38 +0200

    Remove old

--- src.old/dream.ts
+++
@@ -1,7 +0,0 @@
-import { HomeAssistantMQTT } from "./home-assistant-mqtt";
-
-const mqttClient = new HomeAssistantMQTT();
-
-const bluetoothAdapter = createBluetoothAdapter();
-
-bluetoothAdapter.start();
--- src.old/utils/try.ts
+++
@@ -1,7 +0,0 @@
-export async function tryOr<T, S>(defaultValue: T, func: () => Promise<S>) {
-  try {
-    return await func();
-  } catch {
-    return defaultValue;
-  }
-}
